90fc000ba159fe5cae5a2c7711a8446987c56599
Alana Nery <lananery123@gmail.com>
2025-05-01 11:56:09 -0300

    edit projeto2

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function proximaFaixa() {
     capituloAtual = capituloAtual + 1;
     }
 
-    audioCapitulo.src = '/aula 1 (do zero)/books/dom-casmurro/' + capituloAtual + '.mp3';
+    audioCapitulo.src = '/books/dom-casmurro/' + capituloAtual + '.mp3';
     tocarFaixa();
     taTocando = 1;
     trocarNomeFaixa();
@@ -55,7 +55,7 @@ function voltarFaixa() {
     capituloAtual = capituloAtual - 1;
     }
 
-    audioCapitulo.src = '/aula 1 (do zero)/books/dom-casmurro/' + capituloAtual + '.mp3';
+    audioCapitulo.src = '/books/dom-casmurro/' + capituloAtual + '.mp3';
     tocarFaixa();
     taTocando = 1;
     trocarNomeFaixa();
